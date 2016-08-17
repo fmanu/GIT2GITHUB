52f273cd7062f0e290edb90e07617809df7113dd
fred <l00120580@student.lyit.ie>
2016-08-17 14:37:42 +0100

    FIRST COMMIT

---
+++ src/pojos/TstClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pojos;
+
+/**
+ * @author l00120580
+ *
+ */
+public class TstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
